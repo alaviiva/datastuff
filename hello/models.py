afbded85f81037550daae3391884f5673e8f2f4f
Sampo Vartiala <sampo.vartiala@gmail.com>
2019-03-08 18:42:59 +0200

    Add models

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Sensordata(models.Model):
+    date = models.DateTimeField()
+    sensor1 = models.FloatField()
+    sensor2 = models.FloatField()
+    sensor3 = models.FloatField()
+    sensor4 = models.FloatField()
+
