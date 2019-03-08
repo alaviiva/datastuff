f675b91d8e8a981ddc7d07d21c86cd36ad0aedc3
Sampo Vartiala <sampo.vartiala@gmail.com>
2019-03-08 18:44:43 +0200

    Settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,3 +117,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
